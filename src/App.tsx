ae49abb977c83b9bc0ff3e30cd4e343336316699
VitoZMX <Vi-69@yandex.ru>
2023-05-10 13:49:31 +0300

    refactor: change style component (01)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {TaskType, Todolist} from './Todolist'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let tasks1: Array<TaskType> = [
+        {id: 1, title: 'CSS', isDone: true},
+        {id: 2, title: 'HTML', isDone: false},
+        {id: 3, title: 'JS', isDone: true}
+    ]
+    let tasks2: Array<TaskType> = [
+        {id: 1, title: 'Film1', isDone: true},
+        {id: 2, title: 'Film2', isDone: false},
+        {id: 3, title: 'Film3', isDone: false}
+    ]
+    let tasks3: Array<TaskType> = [
+        {id: 1, title: '123', isDone: true},
+        {id: 2, title: '123', isDone: false},
+        {id: 3, title: '123', isDone: false}
+    ]
+
+    return (
+        <div className="App">
+            <Todolist title={'What to learn'} tasks={tasks1}/>
+            <Todolist title={'Movies'} tasks={tasks2}/>
+            <Todolist title={'Plans to weekend'} tasks={tasks3}/>
+        </div>
+    )
 }
 
-export default App;
+export default App
